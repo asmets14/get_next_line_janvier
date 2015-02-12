8289f6ca726dc28f6a20e7b21439d9cc655d9980
Jonathan CHICHEPORTICHE <jchichep@e2r4p15.42.fr>
2015-02-12 12:55:42 +0100

    fonctionel

--- get_next_line.c
+++ get_next_line.c
@@ -6,87 +6,73 @@
 /*   By: asmets <asmets@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/04 13:36:17 by asmets            #+#    #+#             */
-/*   Updated: 2015/02/04 14:17:45 by asmets           ###   ########.fr       */
+/*   Updated: 2015/02/10 04:15:54 by jchichep         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-void remove_from_list(t_list **list)
+int			remove_from_list(t_list **list, char *temp)
 {
-	t_list *tmp;
-	t_list *tmp2;
-	t_rest *truc;
+	t_list	*tmp;
+	t_list	*tmp2;
 
+	if (!temp)
+		return (0);
 	if (*list == NULL)
-		return ;
+		return (1);
 	tmp2 = *list;
 	while (tmp2)
 	{
-		truc = (t_rest *)tmp2->content;
-		printf("TEST6 [%s][%d]\n", truc->str, truc->fd);
-		if (truc->str[0] == '\0')
+		if (((t_rest *)tmp2->content)->str == NULL)
 		{
-			printf("TEST5\n");
-		 	free(truc->str);
-		 	if (tmp2 == *list)
-			 	*list = tmp2->next;
+			((t_rest *)tmp2->content)->fd = -1;
+			free((t_rest *)(tmp2->content));
+			if (tmp2 == *list)
+				*list = tmp2->next;
 			else
 				tmp->next = tmp2->next;
 			free(tmp2);
 		}
-		tmp = tmp2;
+		else
+			tmp = tmp2;
 		tmp2 = tmp2->next;
 	}
+	return (1);
 }
-char *get_from_list(t_list *list, int fd, int *ret)
-{
-	t_rest *truc;
 
+char		*get_from_list(t_list *list, int fd, int *ret, char **buff)
+{
+	t_rest	*truc;
+	char	*str;
+
+	if (fd < 0 || fd == 1 || fd == 2 || (*buff = ft_strnew(BUFF_SIZE)) == NULL)
+		return (NULL + 1);
+	*ret = 0;
 	while (list)
 	{
 		truc = (t_rest *)list->content;
 		if (truc->fd == fd)
 		{
-			*ret = 1;
-			return(ft_strdup(truc->str));
+			str = ft_strdup(truc->str);
+			free(truc->str);
+			truc->str = NULL;
+			return (str);
 		}
 		list = list->next;
 	}
-	*ret = 0;
-	return(NULL);
+	return (NULL);
 }
 
-int put_to_list(t_list **list, int fd, char *str)
+int			add_maillon(t_list **list, t_list *tmp2, int fd, char *str)
 {
-	t_rest *truc;
-	t_list *tmp;
-	t_list *tmp2;
-	t_list *maillon;
+	t_list	*maillon;
+	t_rest	*truc;
 
-	tmp = *list;
-	if (str[0] == '\0')
-		return (0);
-	while (tmp)
-	{		
-		truc = (t_rest *)tmp->content;
-		if (truc->fd == fd)
-		{
-			if (truc->str != NULL)
-			{
-				free(truc->str);
-				truc->str = ft_strdup(str);
-				printf("TEST8\n");
-				return(0);
-			}
-		}
-		tmp2 = tmp;
-		tmp = tmp->next;
-	}
-	if ((maillon = ft_memalloc(sizeof(*maillon))) == NULL || (maillon->content = ft_memalloc(sizeof(t_rest))) == NULL)
+	if ((maillon = ft_memalloc(sizeof(*maillon))) == NULL \
+		|| (maillon->content = ft_memalloc(sizeof(t_rest))) == NULL)
 		return (-1);
-	printf("TEST7\n");	
 	truc = (t_rest *)maillon->content;
 	truc->fd = fd;
 	truc->str = ft_strdup(str);
@@ -98,41 +84,57 @@ int put_to_list(t_list **list, int fd, char *str)
 	return (0);
 }
 
-
-int get_next_line(int const fd, char **line)
+int			put_to_list(t_list **list, int fd, char *str)
 {
-	static t_list *list = NULL;
-	int ret;
-	char *buff;
-	char *temp;
-	char *to_free;
+	t_rest	*truc;
+	t_list	*tmp;
+	t_list	*tmp2;
 
-sleep(1);	
-	temp = get_from_list(list, fd, &ret);
-	if ((buff = ft_strnew(BUFF_SIZE)) == NULL)
-		return (-1);
-	while (((temp == NULL) || (ft_strchr(temp, '\n')) == NULL) && ((ret = read(fd, buff, BUFF_SIZE)) > 0))
+	tmp = *list;
+	if (str == NULL + 1 || str[0] == '\0')
+		return (0);
+	while (tmp)
 	{
-		printf("TEST4, [%d]\n", ret);
+		truc = (t_rest *)tmp->content;
+		if (truc->fd == fd)
+		{
+			if (truc->str != NULL)
+			{
+				free(truc->str);
+				truc->str = ft_strdup(str);
+				return (0);
+			}
+		}
+		tmp2 = tmp;
+		tmp = tmp->next;
+	}
+	return (add_maillon(list, tmp2, fd, str));
+}
+
+int			get_next_line(int const fd, char **line)
+{
+	static t_list	*list = NULL;
+	char			*buff;
+	char			*temp;
+	char			*to_free;
+	int				ret;
+
+	if ((temp = get_from_list(list, fd, &ret, &buff)) == NULL + 1)
+		return (-1);
+	while (((temp == NULL) || (ft_strchr(temp, '\n')) == NULL) \
+		&& ((ret = read(fd, buff, BUFF_SIZE)) > 0))
+	{
 		to_free = temp;
 		temp = ft_strjoin(temp, buff);
+		bzero(buff, BUFF_SIZE);
 		if (to_free != NULL)
 			free(to_free);
 	}
-		printf("TEST1, [%d]\n", ret);
-	if (ret == 0)
-	{
-		printf("TEST2\n");
-		remove_from_list(&list);
-		return (0);
-	}
-	*line = ft_strcdup(temp, '\n');
-	to_free = temp;
-	temp = ft_strchr(temp, '\n') + 1;
-	put_to_list(&list, fd, temp);
-	free(to_free);
 	free(buff);
-	printf("TEST3\n");
-	remove_from_list(&list);
+	if ((ret = (ret == -1 ? -1 : remove_from_list(&list, temp))) < 1)
+		return (ret);
+	*line = ft_strcdup(temp, '\n');
+	put_to_list(&list, fd, ft_strchr(temp, '\n') + 1);
+	free(temp);
 	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,30 +6,25 @@
 /*   By: asmets <asmets@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/04 14:06:58 by asmets            #+#    #+#             */
-/*   Updated: 2015/02/04 14:22:48 by asmets           ###   ########.fr       */
+/*   Updated: 2015/02/10 04:17:06 by jchichep         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-
-
-
-
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include "libft/libft.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft/libft.h"
 
-#define BUFF_SIZE 100
+# define BUFF_SIZE 100
 
-typedef struct s_rest
+typedef struct	s_rest
 {
-	char *str;
-	int fd;
+	char		*str;
+	int			fd;
 }				t_rest;
 
-int get_next_line(int const fd, char **line);
+int				get_next_line(int const fd, char **line);
 
 #endif
